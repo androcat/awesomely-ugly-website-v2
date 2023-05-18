f219ec2130cd8070702b32bbf5f3ece0ab605315
Jay Medina <jay.medina.celis@gmail.com>
2023-05-18 14:36:04 -0400

    Adds functionality to image changing button

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 (function () {
   "use strict";
 
+  let img = document.querySelector(".changeable-img");
+  let cap = document.querySelector(".appearing-caption");
+
   const btn = document.querySelector(".button");
 
-  const displayImg = function () {
-    document.getElementById("add-grug").innerHTML =
-      //   '<marquee direction="up" behavior="alternate"><img src="images/caveppl_fire.png"><caption>Grug family photo</caption></marquee>';
-      '<img src="images/caveppl_fire.png">'; //<caption>Grug family photo</caption>';
+  //   const displayImg = function () {
+  //     document.getElementById("add-grug").innerHTML =
+  //       //   '<marquee direction="up" behavior="alternate"><img src="images/caveppl_fire.png"><caption>Grug family photo</caption></marquee>';
+  //       '<img src="images/caveppl_fire.png">'; //<caption>Grug family photo</caption>';
+  //   };
+
+  const changeImg = function () {
+    img.src = "images/caveppl_fire.png";
   };
 
-  btn.addEventListener("click", displayImg);
+  // Doesn't work yet
+  //   const addCaption = function () {
+  //     cap.innerHTML = "TEXT HERE"
+  //   }
+
+  btn.addEventListener("click", changeImg);
 })();
